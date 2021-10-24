558b7a866f2783c855863573493151d5748476b0
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2021-10-24 13:40:37 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, nuevoColor):
+        if ( nuevoColor == "amarillo" or nuevoColor == "verde" or nuevoColor == "negro" or nuevoColor == "rojo"):
+            self.color = nuevoColor
+        else:
+            self.color = self.color
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro 
+        
+    
+    def cantidadAsientos(self):
+        self.asientos = Asiento()
+        return self.asientos
+    
+    def verificarIntegridad(self):
+        if (self.registro != Auto.registro or self.registro != Motor.registro):
+            print("Las piezas no son originales")
+        else:
+            print("Auto Original")
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, numero):
+        self.registro = numero
+
+    def asignarTipo(self, valor):
+        if (valor == "electrico" or valor == "gasolina"):
+            self.tipo = valor 
+        else:
+            self.tipo = self.tipo 
+
+
